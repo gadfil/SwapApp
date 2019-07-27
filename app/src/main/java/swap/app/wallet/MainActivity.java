f089a411b7eecdd239898c30bca1c94db8b5beae
oleggamega <gadfil12@gmail.com>
2019-07-27 17:43:09 +0300

    Initial commit

---
+++ app/src/main/java/swap/app/wallet/MainActivity.java
@@ -0,0 +1,16 @@
+package swap.app.wallet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.loadUrl("file:///android_asset/build/index.html");
+    }
+}
